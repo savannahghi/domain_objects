2893c23c00f78b908eb65bd1677d1c1be58dc880
John Ngugi <johnngugi777@gmail.com>
2021-03-24 10:42:12 +0300

    chore: initial constructor CommunicationSettings

--- lib/src/entities/communication_settings.dart
+++ lib/src/entities/communication_settings.dart
@@ -1,4 +1,5 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:sil_core_domain_objects/value_objects.dart';
 
 part 'communication_settings.freezed.dart';
 part 'communication_settings.g.dart';
@@ -12,6 +13,14 @@ class CommunicationSettings with _$CommunicationSettings {
     @JsonKey(name: 'allowPush') bool? allowPush,
     @JsonKey(name: 'allowEmail') bool? allowEmail,
   }) = _CommunicationSettings;
+
+  factory CommunicationSettings.initial() => CommunicationSettings(
+        profileID: UNKNOWN,
+        allowWhatsApp: false,
+        allowText: false,
+        allowPush: false,
+        allowEmail: false,
+      );
 
   factory CommunicationSettings.fromJson(Map<String, dynamic> json) =>
       _$CommunicationSettingsFromJson(json);
---
+++ test/communication_settings_test.dart
@@ -0,0 +1,81 @@
+import 'package:sil_core_domain_objects/src/entities/communication_settings.dart';
+import 'package:sil_core_domain_objects/value_objects.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('CommuncationSettings tests', () {
+    test('expect valid CommuncationSettings instance', () {
+      final CommunicationSettings communicationSettings = CommunicationSettings(
+        profileID: UNKNOWN,
+        allowWhatsApp: false,
+        allowText: false,
+        allowPush: false,
+        allowEmail: false,
+      );
+
+      expect(communicationSettings.profileID, UNKNOWN);
+      expect(communicationSettings.allowWhatsApp, false);
+      expect(communicationSettings.allowText, false);
+      expect(communicationSettings.allowPush, false);
+      expect(communicationSettings.allowEmail, false);
+    });
+
+    test('expects a valid CommuncationSettings instance should convert to json',
+        () {
+      final CommunicationSettings communicationSettings = CommunicationSettings(
+        profileID: UNKNOWN,
+        allowWhatsApp: false,
+        allowText: false,
+        allowPush: false,
+        allowEmail: false,
+      );
+
+      final Map<String, dynamic> json = communicationSettings.toJson();
+
+      expect(json, isA<Map<String, dynamic>>());
+      expect(json['profileID'], UNKNOWN);
+      expect(json['allowWhatsApp'], false);
+      expect(json['allowTextSMS'], false);
+      expect(json['allowPush'], false);
+      expect(json['allowEmail'], false);
+    });
+
+    test(
+        'expects a valid CommunicationSettings instance should convert from '
+        'json', () {
+      const String profileId = 'ff83b587-d78a-498f-b0df-4d8cc9d4eb04';
+      const String id = 'c185150b-7422-4b3f-a16d-248a8cbe7aed';
+      final Map<String, dynamic> communicationSettingsJson = <String, dynamic>{
+        'allowEmail': true,
+        'allowPush': false,
+        'allowTextSMS': true,
+        'allowWhatsApp': false,
+        'id': id,
+        'profileID': profileId
+      };
+
+      final CommunicationSettings communicationSettings =
+          CommunicationSettings.fromJson(communicationSettingsJson);
+      expect(communicationSettings, isNotNull);
+
+      expect(communicationSettings.profileID, profileId);
+      expect(communicationSettings.allowWhatsApp, false);
+      expect(communicationSettings.allowText, true);
+      expect(communicationSettings.allowPush, false);
+      expect(communicationSettings.allowEmail, true);
+    });
+
+    test(
+        'expects CommunicationSettings.initial should return a valid '
+        'CommunicationSettings instance with initial values', () {
+      final CommunicationSettings communicationSettings =
+          CommunicationSettings.initial();
+
+      expect(communicationSettings.profileID, UNKNOWN);
+      expect(communicationSettings.allowWhatsApp, false);
+      expect(communicationSettings.allowText, false);
+      expect(communicationSettings.allowPush, false);
+      expect(communicationSettings.allowEmail, false);
+    });
+  });
+}
