84ae7700e8f028dc2270d85de6275272e39d1470
Kevin Maiyo <47208315+maiyo01@users.noreply.github.com>
2021-07-28 12:41:26 +0300

    feat: add error to exception class (#13)

--- lib/src/failures/exception.dart
+++ lib/src/failures/exception.dart
@@ -1,6 +1,7 @@
 /// Custom Exception class
 class SILException implements Exception {
-  SILException({required this.cause, required this.message});
+  SILException({required this.cause, required this.message, this.error});
   final dynamic message;
   final dynamic cause;
+  final dynamic error;
 }
